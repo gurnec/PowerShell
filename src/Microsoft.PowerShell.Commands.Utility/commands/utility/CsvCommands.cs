53afbeab3b0563b0d6d76b32625baff71e758468
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-11-09 23:31:23 -0500

    :art: Remove empty line for codefactor

--- src/Microsoft.PowerShell.Commands.Utility/commands/utility/CsvCommands.cs
+++ src/Microsoft.PowerShell.Commands.Utility/commands/utility/CsvCommands.cs
@@ -1034,7 +1034,6 @@ namespace Microsoft.PowerShell.Commands
                 // If value is null, assume property is not present and skip it.
                 if (value != null)
                 {
-
                     if (_quoteFields != null)
                     {
                         if (_quoteFields.TryGetValue(propertyName, out _))
